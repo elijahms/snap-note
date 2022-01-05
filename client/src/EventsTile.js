4d7033879a1d4ad67636644545f07a6e0e8d487f
Elijah Silverman <elijahmsilverman@gmail.com>
2022-01-05 14:13:16 -0500

    additional changes

--- client/src/EventsTile.js
+++ client/src/EventsTile.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import Notes from './Notes'
 import {Box, Button, Stack, Container} from '@mui/material'
 
 const EventsTile = ({name}) => {
+    
+    const [userNotes, setUserNotes] = useState({
+        content: "no notes to show"
+    }) 
+    const [noteClick, setNoteClick] = useState(false)
+
+    useEffect(() => {
+        fetch("/api/notes")
+        .then((r) => r.json())
+        .then((data) => {
+            setUserNotes(data)
+            console.log(data)
+            }
+        )
+      }, []);
+    
     return (
         <Container className='event-tile-container'>
-            <Button className='event-tile-name' variant='contained'>{name}</Button>
+            <Button onClick={(e) => setNoteClick(() => !noteClick)} className='event-tile-name' variant='contained'>{name}</Button>
+            <Stack>
+            {noteClick && userNotes.map((n) => <Notes key={n.id} content={n.content} />)}
+            </Stack>
         </Container>
     )
 }
---
+++ client/src/Notes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notes = ({content}) => {
+    return (
+        <div>
+            <p>{content}</p>
+        </div>
+    )
+}
+
+export default Notes
