43815bb7028f489ba41e58d5e211895186e2c173
Andrew Busel <buselax@gmail.com>
2022-01-05 13:52:56 -0500

    changed migration so note content field is text and not string

--- db/migrate/20220105185204_create_notes.rb
+++ db/migrate/20220105185204_create_notes.rb
@@ -1,7 +1,7 @@
 class CreateNotes < ActiveRecord::Migration[7.0]
   def change
     create_table :notes do |t|
-      t.string :content
+      t.text :content
       t.integer :event_id
 
       t.timestamps
