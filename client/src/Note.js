5c8fdd6c4c63c660ad09bece69930c95b4bd4247
Elijah Silverman <elijahmsilverman@gmail.com>
2022-01-05 21:27:00 -0500

    added delete icon

--- client/src/Note.js
+++ client/src/Note.js
@@ -4,16 +4,24 @@ import ListItem from '@mui/material/ListItem';
 import List from '@mui/material/List';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
 
 const Note = ({content, setSelectedNote, id, created_at}) => {
     return (
         <List>
-            <ListItem 
+            <ListItem
+                secondaryAction={
+                <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                </IconButton>
+                }
             button
             onClick = {(e) => setSelectedNote({
                 content: content,
-                id: id})}>
-            <ListItemText primary={JSON.parse(content).blocks[0].text} secondary={'created: ' + created_at} />
+                id: id})}
+            >
+            <ListItemText primary={JSON.parse(content).blocks[0].text} secondary={'created: ' + created_at}/>            
             </ListItem>
             <Divider />
         </List>
