18d4e670f55f81ef437110322927e2f32f66460c
Elijah Silverman <elijahmsilverman@gmail.com>
2022-01-06 13:31:40 -0500

    func is done

--- client/src/EditNote.js
+++ client/src/EditNote.js
@@ -1,25 +1,31 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { Editor } from "react-draft-wysiwyg";
 import {EditorState} from 'draft-js'
 import { convertToRaw } from "draft-js";
 import { convertFromRaw } from "draft-js";
 import {MyButton} from './Components/Button.style.js'
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+import Stack from '@mui/material/Stack'
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 
 const EditNote = ({selectedNote, setSelectedNote}) => {
     
     const [editorState, setEditorState] = useState(() => EditorState.createWithContent(convertFromRaw(JSON.parse(selectedNote.content))));
 
-  const wrapperStyle = {
-    border: '1px solid #969696',
-  }
+    const cmdPress = useKeyPress("Meta");
+    const sPress = useKeyPress("s");
+    cmdPress && sPress && newNoteFunc()
+
+    const wrapperStyle = {
+      borderRadius: '4px'
+    }
   
-  
-  const editorStyle = {
-    height:'10rem',
-    padding:'1rem'
-  }
+    const editorStyle = {
+      height:'30rem',
+      padding:'1rem'
+    }
 
   function newNoteFunc() {
     fetch(`/api/notes/${selectedNote.id}`, {
@@ -40,15 +46,39 @@ const EditNote = ({selectedNote, setSelectedNote}) => {
   }
 
     return (
-        <div>
-            <Editor
-               toolbarOnFocus
+          <div>
+          <Stack spacing={2}
+          >
+            {/* <Box
+            // sx={{boxShadow: 3, borderRadius: 2}}
+            > */}
+              <Editor
+              toolbar={{
+                options: ['inline', 'blockType', 'fontSize', 'fontFamily'],
+                inline: {
+                  options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace'],
+                  bold: { className: 'bordered-option-classname' },
+                  italic: { className: 'bordered-option-classname' },
+                  underline: { className: 'bordered-option-classname' },
+                  strikethrough: { className: 'bordered-option-classname' },
+                  code: { className: 'bordered-option-classname' },
+                },
+                blockType: {
+                  className: 'bordered-option-classname',
+                },
+                fontSize: {
+                  className: 'bordered-option-classname',
+                },
+                fontFamily: {
+                  className: 'bordered-option-classname',
+                },
+              }}
                editorState={editorState}
                onEditorStateChange={setEditorState}
                wrapperStyle={wrapperStyle}
                editorStyle={editorStyle}   
               />
-          {/* </div> */}
+              {/* </Box> */}
           <MyButton
             onClick={newNoteFunc}
             variant="contained"
@@ -59,8 +89,44 @@ const EditNote = ({selectedNote, setSelectedNote}) => {
             variant="contained"
             >Back
           </MyButton>
-        </div>
+          </Stack>
+          </div>
     )
 }
+
+function useKeyPress(targetKey) {
+  // State for keeping track of whether key is pressed
+  const [keyPressed, setKeyPressed] = useState(false);
+  // If pressed key is our target key then set to true
+  function downHandler({ key }) {
+    if (key === targetKey) {
+      console.log(key)
+      setKeyPressed(true);
+    }
+  }
+  // If released key is our target key then set to false
+  const upHandler = ({ key }) => {
+    if (key === targetKey) {
+      setKeyPressed(false);
+    }
+  };
+  // Add event listeners
+  useEffect(() => {
+    window.addEventListener("keydown", downHandler);
+    window.addEventListener("keyup", upHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      window.removeEventListener("keyup", upHandler);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+  return keyPressed;
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === 's' && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+    e.preventDefault();
+  }
+}, false);
 
 export default EditNote
--- client/src/NewNote.js
+++ client/src/NewNote.js
@@ -1,6 +1,6 @@
 import {MyButton} from './Components/Button.style.js'
 import Container  from '@mui/material/Container'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Editor } from "react-draft-wysiwyg";
 import {EditorState} from 'draft-js'
 import { convertToRaw } from "draft-js";
@@ -9,11 +9,16 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 import Box from '@mui/material/Box';
 
 
+
 const NewNote = ({user}) => {
 
 
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
-  const [isNew, setIsNew] = useState(true)
+  const [selectedNote, setSelectedNote] = useState(false)
+  
+  const cmdPress = useKeyPress("Meta");
+  const sPress = useKeyPress("s");
+  cmdPress && sPress && newNoteFunc()
 
   const wrapperStyle = {
     borderRadius: '4px'
@@ -26,30 +31,39 @@ const NewNote = ({user}) => {
 
   function newNoteFunc() {
     // console.log(JSON.stringify(convertToRaw(editorState.getCurrentContent())))
+  if (!selectedNote) {
     fetch("/api/notes", {
-      method: "POST",
-      headers: {
-          "Content-Type" : "application/json"
-      },
-      body:  JSON.stringify({content: JSON.stringify(convertToRaw(editorState.getCurrentContent()))})
-    })
-    .then((r) => {
-      if (r.ok) {
-        r.json().then((r) => console.log(r));
-      } else {
-        r.json().then((err) => console.log(err));
-      }
-    });
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body:  JSON.stringify({content: JSON.stringify(convertToRaw(editorState.getCurrentContent()))})
+      })
+      .then((r) => {
+        if (r.ok) {
+          r.json().then((r) => setSelectedNote(r.id));
+        } else {
+          r.json().then((err) => console.log(err));
+        }
+      })
+    } else {
+      fetch(`/api/notes/${selectedNote}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+       
+        body:  JSON.stringify({content: JSON.stringify(convertToRaw(editorState.getCurrentContent()))})
+      })
+      .then((r) => {
+        if (r.ok) {
+          r.json().then((r) => console.log(r));
+        } else {
+          r.json().then((err) => console.log(err));
+        }
+      });
+    }
   }
-
-  // document.addEventListener("keydown", function(e) {
-  //   if (isNew && e.key === 's' && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-  //     console.log('yo')
-  //     e.preventDefault();
-  //     newNoteFunc()
-  //     setIsNew(false)
-  //   }
-  // }, false);
     
     return (
         <Container component='main' maxWidth="lg" 
@@ -99,5 +113,40 @@ const NewNote = ({user}) => {
         </Container>
     )
 }
+
+function useKeyPress(targetKey) {
+  // State for keeping track of whether key is pressed
+  const [keyPressed, setKeyPressed] = useState(false);
+  // If pressed key is our target key then set to true
+  function downHandler({ key }) {
+    if (key === targetKey) {
+      console.log(key)
+      setKeyPressed(true);
+    }
+  }
+  // If released key is our target key then set to false
+  const upHandler = ({ key }) => {
+    if (key === targetKey) {
+      setKeyPressed(false);
+    }
+  };
+  // Add event listeners
+  useEffect(() => {
+    window.addEventListener("keydown", downHandler);
+    window.addEventListener("keyup", upHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      window.removeEventListener("keyup", upHandler);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+  return keyPressed;
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === 's' && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+    e.preventDefault();
+  }
+}, false);
 
 export default NewNote;
